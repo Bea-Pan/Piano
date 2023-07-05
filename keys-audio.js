c69e0bd780eb56a192d7220602bc0978b7df6ace
Bea Pan <bpankowska@gmail.com>
2023-07-05 20:10:23 +0300

    added sounds, changed colors

---
+++ keys-audio.js
@@ -0,0 +1,64 @@
+//white keys
+const C3 = new Audio();
+C3.src ="./media/C3.mp3";
+
+const D3 = new Audio();
+D3.src ="./media/D3.mp3";
+
+const E3 = new Audio();
+E3.src ="./media/E3.mp3";
+
+const F3 = new Audio();
+F3.src ="./media/F3.mp3";
+
+const G3 = new Audio();
+G3.src ="./media/G3.mp3";
+
+const A3 = new Audio();
+A3.src ="./media/A3.mp3";
+
+const B3 = new Audio();
+B3.src ="./media/B3.mp3";
+
+const C4 = new Audio();
+C4.src ="./media/C4.mp3";
+
+const D4 = new Audio();
+D4.src ="./media/D4.mp3";
+
+const E4 = new Audio();
+E4.src ="./media/E4.mp3";
+
+const F4 = new Audio();
+F4.src ="./media/F4.mp3";
+
+const G4 = new Audio();
+G4.src ="./media/G4.mp3";
+
+const A4 = new Audio();
+A4.src ="./media/A4.mp3";
+
+const B4 = new Audio();
+B4.src ="./media/B4.mp3";
+
+const C5 = new Audio();
+C5.src ="./media/C5.mp3";
+
+const D5 = new Audio();
+D5.src ="./media/D5.mp3";
+
+const E5 = new Audio();
+E5.src ="./media/E5.mp3";
+
+const F5 = new Audio();
+F5.src ="./media/F5.mp3";
+
+const G5 = new Audio();
+G5.src ="./media/G5.mp3";
+
+const A5 = new Audio();
+A5.src ="./media/A5.mp3";
+
+const B5 = new Audio();
+B5.src ="./media/B5.mp3";
+
